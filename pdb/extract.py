3960cc7b872bb6241b86d7a004d33f14beb2a9f1
Sven Fillinger <sven.fillinger@gmail.com>
2015-05-18 16:50:54 +0200

    Added functionality to the get_secondary_structure_annotation() method.
    Use the main() to test it, works fine for me

--- pdb/extract.py
+++ pdb/extract.py
@@ -4,7 +4,7 @@ the BioPython library.
 """
 from Bio import PDB
 
-from . import constants
+import constants
 
 
 def get_amino_acids(ident, file):
@@ -29,13 +29,32 @@ def get_secondary_structure_annotation(file):
     right handed helices and beta sheets. We need the residue positions and its
     annotated structure for calculating torsion angles for only these amino
     acids.
-    Unfortunately, the PDB-modul from BioPython does not support secondary
+    Unfortunately, the PDB-module from BioPython does not support secondary
     structure read out so far...
     :param file: the path to the pdb-file
-    :return: a touple of two lists of residue positions in [0]: helices
-                                                           [1]: beta sheets
+    :return: a tuple of two lists of residue positions in [0]: helices
+                                                          [1]: beta sheets
     """
-    return -1, -1
+    pdb_file = open(file, "r")
+    record_sheet_aa = []  # A list with residue positions in sheets
+    record_helix_aa = []  # A list with residue positions in helices
+    for line in pdb_file.readlines():
+        # if line provides a HELIX information
+        if "HELIX" in line[0:5] and "A" in line[19]:
+            # we want only the residues of one chain to avoid duplicates
+            # check for helix class
+            if int(line[39:40]) == 1:
+                # '1' is default alpha helix
+                # record the aminoacid residue position in a helix
+                record_helix_aa += range(int(line[21:25]),
+                                         int(line[33:37]) + 1)
+        elif "SHEET" in line[0:5] and "A" in line[21]:
+            # if line provides a SHEET information
+            start_res = int(line[22:26])  # the start residue of a sheet
+            term_res = int(line[33:37])  # the terminating res. of a sheet
+            record_sheet_aa += range(start_res, term_res, 1)
+    pdb_file.close()
+    return record_helix_aa, record_sheet_aa
 
 
 def get_backbone_torsion_angles(generator_aa):
@@ -44,8 +63,14 @@ def get_backbone_torsion_angles(generator_aa):
     beta-sheets from a Generator with all amino acid residues from a PDB-file
     :param generator_aa: Generator with extracted amino acid residues from the
     PDB-file
-    :return: Two list of touples, containing the phi and psi torsion angles for
-    alpha-helices
+    :return: Two lists of touples, containing the phi and psi torsion angles
+    for alpha-helices and beta-sheets
     """
     current_residue = generator_aa.next()
     return -1
+
+
+if __name__ == "__main__":
+    # test file
+    pdb_file = "/home/fillinger/git/bioinformatics2/assignment_2/pdb/3VSU.pdb"
+    print get_secondary_structure_annotation(pdb_file)
