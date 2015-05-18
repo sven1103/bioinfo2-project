bf37d4b88df23ec7d976f6988df971a7b2f0348b
sven <sven.fillinger@mailbox.org>
2015-05-18 22:25:11 +0000

    Finished the torsion angle methods. They work fine. Also the resulting angles are nice.
    We have to keep in mind, that the starting/ending residues may have different torsion angles (you can see that in the 3VSU.pdb i.e. where the first psi angles looks weird. This is consistent with the 3d structure, i checked.

--- pdb/extract.py
+++ pdb/extract.py
@@ -3,9 +3,12 @@ Extract some required attributes from PDB files using
 the BioPython library.
 """
 from Bio import PDB
-
+from Bio import BiopythonWarning
+import math
 import constants
-
+import warnings
+# remove the ugly warning from the PDB module, that chains are discontinuous
+warnings.simplefilter("ignore", BiopythonWarning)
 
 def get_amino_acids(ident, file):
     """
@@ -14,11 +17,9 @@ def get_amino_acids(ident, file):
     :param file: Path or File handle to PDB file.
     :return: Generator of all Amino Acid residues of PDB file
     """
-
     struct = PDB.PDBParser().get_structure(ident, file)
 
     for res in struct.get_residues():
-
         if res.get_resname() in constants.AMINO_ACIDS:
             yield res
 
@@ -57,26 +58,71 @@ def get_secondary_structure_annotation(file):
     return record_helix_aa, record_sheet_aa
 
 
+def compute_torsion_angles(residue, next_residue):
+    """
+    Little helper function, calculates the backbone phi and psi torsion
+    angles from the given residues and returns them
+    :param residue: The amino acid residue the torsion angles shall be computed
+    :return: Phi and psi backbone torsion angles
+    """
+    # extract the atoms for the torsion calculation
+    # 1.) for the phi
+    atom_CO_1 = residue['C'].get_vector()
+    atom_N_1 = residue['N'].get_vector()
+    atom_CA_1 = residue['CA'].get_vector()
+    atom_CA_2 = next_residue['CA'].get_vector()
+    atom_CO_2 = next_residue['C'].get_vector()
+    atom_N_2 = next_residue['N'].get_vector()
+
+    phi_angle = PDB.calc_dihedral(atom_CO_1, atom_N_2, atom_CA_2, atom_CO_2)
+    psi_angle = PDB.calc_dihedral(atom_N_1, atom_CA_1, atom_CO_1, atom_N_2)
+
+    # convert into degrees
+    return math.degrees(phi_angle), math.degrees(psi_angle)
+
+
 def get_backbone_torsion_angles(generator_aa, pos_helix, pos_sheet):
     """
     Calculates the backbone torsion angles for annotated alpha-helices and
     beta-sheets from a Generator with all amino acid residues from a PDB-file
     :param generator_aa: Generator with extracted amino acid residues from the
     PDB-file
-    :return: Two lists of touples, containing the phi and psi torsion angles
+    :return: Two lists of tuples, containing the phi and psi torsion angles
     for alpha-helices and beta-sheets
     """
-    for item in generator_aa:
-        print item.atom()
-    return -1
+    torsion_angles_helix = []
+    torsion_angles_sheet = []
+
+    for residue in generator_aa:
+        residue_pos = residue.get_id()[1]
+        chain = residue.get_full_id()[2]
+        if chain is not 'A':
+            break
+        next_residue = generator_aa.next()
+        if residue_pos in pos_helix and next_residue.get_id()[1] in pos_helix:
+            torsion_angles_helix.append(compute_torsion_angles(residue,
+                                                               next_residue))
+        elif residue_pos in pos_sheet and next_residue.get_id()[1] in pos_sheet:
+            torsion_angles_sheet.append(compute_torsion_angles(residue,
+                                                               next_residue))
+    return torsion_angles_helix, torsion_angles_sheet
 
 
 if __name__ == "__main__":
     # test file
-    pdb_file = "/home/fillinger/git/bioinformatics2/assignment_2/pdb/3VSU.pdb"
+    pdb_file = "/home/sven/Git/bioinformatics2/assignment_2/pdb/3VSU.pdb"
+    #pdb_file = "/home/fillinger/git/bioinformatics2/assignment_2/pdb/3VSU.pdb"
     residues = get_amino_acids("test", pdb_file)
     # example: print the residues that are in helices
     structure_positions = get_secondary_structure_annotation(pdb_file)
     print structure_positions[0]
-    get_backbone_torsion_angles(residues, structure_positions[0],
-                                structure_positions[1])
+    res = get_backbone_torsion_angles(residues, structure_positions[0],
+                                      structure_positions[1])
+    # alpha helices torsions
+    for angles in res[0]:
+        print angles[0], ":", angles[1]
+
+    # beta sheet torsions
+    print "-----------------"
+    for angles in res[1]:
+        print angles[0], ":", angles[1]
