ce774a1a0cea86e5a7af8a97f1fc9744db31160d
lukas <luk.zim91@gmail.com>
2015-05-29 11:20:47 +0200

    Added some required constants. You can now compute hydrogen bonds between aas using h_bonds of dssp.py The function is not yet pretty well tested, so there might occur errors for some PDB files

--- pdb/constants.py
+++ pdb/constants.py
@@ -1,7 +1,14 @@
-__author__ = 'lukas'
-
 AMINO_ACIDS = ["ALA", "ARG", "ASN", "ASP", "CYS", "GLU", "GLN", "GLY", "HIS",
                "ILE", "LEU", "LYS", "MET", "PHE", "PRO", "SER", "THR", "TRP",
                "TYR", "VAL"]
 
 NMR = 'nmr'
+
+
+# Hydrogen bond potential
+
+#UNIT_ELECTRON_CHARGE = 1.60217657e-19  # Coulomb
+Q1 = 0.42  #  * UNIT_ELECTRON_CHARGE
+Q2 = 0.20  #  * UNIT_ELECTRON_CHARGE
+DIMENSIONAL_FACTOR = 332
+HBOND_THRESHOLD = -0.5  # kcal/mole
