0015a9afe45dc27cf544ed1489d66be05a1666a9
lukas <luk.zim91@gmail.com>
2015-06-05 10:57:12 +0200

    Added helix class encoding to target_encoding.

--- learn/target_encoding.py
+++ learn/target_encoding.py
@@ -6,3 +6,6 @@ As far as i know numbers can be arbitrary chosen, as long as they are unique
 and integer?
 """
 TARGET_CODES = {"x-s-s": 1, "s-s-s": 2, "s-s-x": 3, "x-x-x": -1}
+
+# class encoding used for helix prediction
+HELIX_CODES = {'No Helix': 0, 'Alpha': 1, 'Pi': 2, '310': 3}
